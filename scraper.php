73720938d72bb69fc0f35cc6b614059f7269d1b9
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-12 14:17:03 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -6,7 +6,7 @@ require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
 $BaseLink = 'http://202.61.43.40:8082/cases/search-result?CasesSearch%5BCASENAMECODE%5D=&CasesSearch%5BCASENO%5D=&CasesSearch%5BCASEYEAR%5D=&CasesSearch%5BCIRCUITCODE%5D=1&CasesSearch%5BMATTERCODE%5D=&CasesSearch%5BPARTY%5D=&CasesSearch%5BGOVT_AGENCY_CODE%5D=&CasesSearch%5BFIRNO%5D=&CasesSearch%5BFIRYEAR%5D=&CasesSearch%5BPOLICESTATIONCODE%5D=&CasesSearch%5BADVOCATECODE%5D=&CasesSearch%5BisPending%5D=3&page=';
 //2&per-page=15
-for($i = 4932; $i < 15700; $i++)
+for($i = 0; $i < 15638; $i++)
 {
 $Newlink = $BaseLink . $i. '&per-page=15';
 $link = file_get_html($Newlink);  

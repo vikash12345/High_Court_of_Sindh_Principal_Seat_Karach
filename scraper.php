82848ff0aebf5a8bbe82db7338a34ecbec841287
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-01-17 13:13:48 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -2,14 +2,15 @@
 // This is a template for a PHP scraper on morph.io (https://morph.io)
 // including some code snippets below that you should find helpful
 
+//require 'scraperwiki.php';
+//require 'scraperwiki/simple_html_dom.php';
 require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
 
-
 $Link = 'http://202.61.43.40:8082/cases/search-result?CasesSearch%5BCASENAMECODE%5D=&CasesSearch%5BCASENO%5D=&CasesSearch%5BCASEYEAR%5D=&CasesSearch%5BCIRCUITCODE%5D=1&CasesSearch%5BMATTERCODE%5D=&CasesSearch%5BPARTY%5D=&CasesSearch%5BGOVT_AGENCY_CODE%5D=&CasesSearch%5BFIRNO%5D=&CasesSearch%5BFIRYEAR%5D=&CasesSearch%5BPOLICESTATIONCODE%5D=&CasesSearch%5BADVOCATECODE%5D=&CasesSearch%5BisPending%5D=3&page=1&per-page=15';
 
-$link =	file_get_html($Link);
-echo $link;
+$abc =	file_get_html($Link);
+echo $abc;
 
 
 ?>
